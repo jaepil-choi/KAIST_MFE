0ac785e475e471e666fe55b8a281ea9cd71df61d
jaepil-choi <chljeffreyz@gmail.com>
2024-09-04 14:53:58 +0900

    [BAF633] : 시뮬레이션 숙제 TODO 입력, 기초 MCS 실습

--- BAF633시뮬레이션/MCS 실습 예제 - Vanilla 옵션 평가-20240903/vanilla_option_example.py
+++ BAF633시뮬레이션/MCS 실습 예제 - Vanilla 옵션 평가-20240903/vanilla_option_example.py
@@ -19,3 +19,6 @@ from mcs_0 import mcprice
 nsim = 1000000
 mc_price = mcprice(s,k,r,q,t,sigma,nsim,flag)
 print(f"MC Price = {mc_price:0.6f}")
+
+# TODO: 가격의 std error와 신뢰구간을 구해보자. 100번 iter 해서 그 안에 95번 들어오는지도 check. 
+# TODO: BS 공식에서 0~T가 아니라 j에서 k 막 이러면 식은 어떻게 바뀌어야 하나? 
